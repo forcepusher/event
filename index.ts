df20bc24e2c793920100e6eea945b3ed28f37236
forcepusher <bionitsoup@gmail.com>
2021-04-05 22:59:10 +0700

    Better folder structure

--- index.ts
+++ index.ts
@@ -1,3 +1,1 @@
-export { default as Event } from './Event';
-export { default as EventSource } from './EventSource';
-export { default as EventTarget } from './EventTarget';
+export * from './lib';
---
+++ lib/index.ts
@@ -0,0 +1,3 @@
+export { default as EventSource } from './EventSource';
+export { default as EventTarget } from './EventTarget';
+export { default as Event } from './Event';
