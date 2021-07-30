a618080159e490de936f8caf46367e63d76a9f57
forcepusher <bionitsoup@gmail.com>
2021-07-30 23:16:04 +0700

    Removing pointless index.ts redirection.

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-export { default } from './lib';
-export * from './lib';
+export { default } from './lib/Event';
+export { default as EventSource } from './lib/EventSource';
+export { default as EventTarget } from './lib/EventTarget';
--- lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { default } from './Event';
-export { default as EventSource } from './EventSource';
-export { default as EventTarget } from './EventTarget';
